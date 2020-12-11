149d1dfc67cbbb18c9aa839c38a4f6db5db07fde
Pete McGranaghan <pete.mcgranaghan@outlook.com>
2020-12-11 18:33:51 +0000

    Solved YourOrderPlease

---
+++ CodeWars/YourOrderPlease.Tests/KataTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YourOrderPlease.Tests
+{
+    [TestClass]
+    public class KataTests
+    {
+        [TestMethod]
+        public void SampleTests()
+        {
+            Assert.AreEqual("Thi1s is2 3a T4est", Kata.Order("is2 Thi1s T4est 3a"));
+            Assert.AreEqual("Fo1r the2 g3ood 4of th5e pe6ople", Kata.Order("4of Fo1r pe6ople g3ood th5e the2"));
+            Assert.AreEqual("", Kata.Order(""));
+        }
+    }
+}
---
+++ CodeWars/YourOrderPlease/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace YourOrderPlease
+{
+    public static class Kata
+    {
+        public static string Order(string words)
+        {
+            IEnumerable<string> wordList = words.Split(' ');
+
+            IOrderedEnumerable<string> orderedList = wordList.OrderBy(word => word.FirstOrDefault(chr => char.IsDigit(chr)));
+
+            return string.Join(' ', orderedList);
+        }
+    }
+}
