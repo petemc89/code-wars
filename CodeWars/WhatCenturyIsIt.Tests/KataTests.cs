0162582427b43ce716ee809110890a41b7191ab5
Pete McGranaghan <pete.mcgranaghan@outlook.com>
2020-12-13 09:09:18 +0000

    Solves WhatCentury

---
+++ CodeWars/WhatCenturyIsIt.Tests/KataTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace WhatCenturyIsIt.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("20th", Kata.WhatCentury("1999"), "With input '1999' solution produced wrong output.");
+            Assert.AreEqual("21st", Kata.WhatCentury("2011"), "With input '2011' solution produced wrong output.");
+            Assert.AreEqual("22nd", Kata.WhatCentury("2154"), "With input '2154' solution produced wrong output.");
+            Assert.AreEqual("23rd", Kata.WhatCentury("2259"), "With input '2259' solution produced wrong output.");
+        }
+    }
+}
---
+++ CodeWars/WhatCenturyIsIt/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WhatCenturyIsIt
+{
+    public class Kata
+    {
+        public static string WhatCentury(string year)
+        {
+            int milleniumCenturyDigits = int.Parse(year.Substring(0, 2));
+
+            int century = year.Substring(1, 3) == "000" ? milleniumCenturyDigits : milleniumCenturyDigits + 1;
+
+            return DisplayWithSuffix(century);
+        }
+
+        private static string DisplayWithSuffix(int num)
+        {
+            //Inspired by https://stackoverflow.com/a/19553611/11492151
+
+            string number = num.ToString();
+
+            if (number.EndsWith("11")) return number + "th";
+            if (number.EndsWith("12")) return number + "th";
+            if (number.EndsWith("13")) return number + "th";
+            if (number.EndsWith("1")) return number + "st";
+            if (number.EndsWith("2")) return number + "nd";
+            if (number.EndsWith("3")) return number + "rd";
+
+            return number + "th";
+        }
+    }
+}
