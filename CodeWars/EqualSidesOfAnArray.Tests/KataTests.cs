bc0e8578271f09c55989fb4657bd22f2889cad45
Pete McGranaghan <pete.mcgranaghan@outlook.com>
2020-12-13 11:22:46 +0000

    Solves EqualSidesOfAnArray

---
+++ CodeWars/EqualSidesOfAnArray.Tests/KataTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace EqualSidesOfAnArray.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(3, Kata.FindEvenIndex(new int[] { 1, 2, 3, 4, 3, 2, 1 }));
+            Assert.AreEqual(1, Kata.FindEvenIndex(new int[] { 1, 100, 50, -51, 1, 1 }));
+            Assert.AreEqual(-1, Kata.FindEvenIndex(new int[] { 1, 2, 3, 4, 5, 6 }));
+            Assert.AreEqual(3, Kata.FindEvenIndex(new int[] { 20, 10, 30, 10, 10, 15, 35 }));
+        }
+    }
+}
---
+++ CodeWars/EqualSidesOfAnArray/Kata.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace EqualSidesOfAnArray
+{
+    public class Kata
+    {
+        public static int FindEvenIndex(int[] arr)
+        {
+            // Create an array of incremental sums
+            int[] incrementalArr = new int[arr.Length];
+            int sum = 0;
+            for (var i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+                incrementalArr[i] = sum;
+            }
+
+            // Reverse the array and loop through
+            int[] reversedArr = arr.Reverse().ToArray();
+            int reverseSum = 0;
+            for (var i = 0; i < reversedArr.Length; i++)
+            {                
+                // Increment a sum
+                reverseSum += reversedArr[i];
+
+                // Compare the sum with the reverse index (aka original index) of the incremented array(length - this.index)
+                int originalIndex = incrementalArr.Length - i - 1;
+                if (reverseSum == incrementalArr[originalIndex])
+                {
+                    // Return the original index
+                    return originalIndex;
+                }
+            }
+
+            // Or return -1 if no matches found
+            return -1;
+        }
+    }
+}
